833e8b189c7348dd843760397e696b4f83c72f42
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-10-08 01:20:58 -0300

    ahora el back esta en prod de vercel

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -232,7 +232,7 @@ const Checkout = () => {
     try {
       let response = await axios.post(
         //"http://localhost:8080/create_preference",
-        "http://backecommerce2-8dwzt6soj-michaelrodriguezuy.vercel.app/create_preference",
+        "https://backecommerce2-crlvcdaye-michaelrodriguezuy.vercel.app/create_preference",
         {
           items: items,
           shipment_cost: parseFloat(shipmentCost),
